92288dca9fed023c2a534c617170fc9ecb6b6901
RikkyM <rikky.mahendra54@gmail.com>
2025-12-18 01:45:44 +0700

    changes: halaman homepage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "leaflet/dist/leaflet.css";
+import './lib/leaflet.setup'
 import AppProviders from "./providers/AppProvider.tsx";
 import { AppRoutes } from "./routes/AppRoutes.tsx";
 
