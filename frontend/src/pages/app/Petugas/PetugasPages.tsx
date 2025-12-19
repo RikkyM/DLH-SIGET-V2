6eed41bcfb99baaaf95d7f5c4dbc67f4efb91188
RikkyM <rikky.mahendra54@gmail.com>
2025-12-19 10:22:28 +0700

    add: dashboard

--- frontend/src/pages/app/Petugas/PetugasPages.tsx
+++ frontend/src/pages/app/Petugas/PetugasPages.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+
 
 const PetugasPages = () => {
   return (
     <section className="flex flex-col gap-3 p-3">
         <div className="h-full rounded-lg border border-gray-300 bg-white p-3 shadow">
-            
+
         </div>
     </section>
   )
