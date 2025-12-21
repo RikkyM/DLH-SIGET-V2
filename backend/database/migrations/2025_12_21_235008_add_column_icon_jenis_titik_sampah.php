700fad0652084382f6b63cc16a4d2cb9f89228df
RikkyM <rikky.mahendra54@gmail.com>
2025-12-22 06:52:29 +0700

    fix: homepage, dashboard, petugas, penampungan sementara

---
+++ backend/database/migrations/2025_12_21_235008_add_column_icon_jenis_titik_sampah.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('jenis_titik_sampah', function (Blueprint $table) {
+            $table->after('nama', function (Blueprint $table) {
+                $table->string('icon')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('jenis_titik_sampah', function (Blueprint $table) {
+            $table->after('nama', function (Blueprint $table) {
+                $table->string('icon')->nullable();
+            });
+        });
+    }
+};
